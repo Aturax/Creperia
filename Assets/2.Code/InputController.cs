8085e629a2082175914fe4b9b49855baf98aea45
Wendy Rivero <60824265+RiveroWendy@users.noreply.github.com>
2023-03-02 14:14:56 -0300

    Trigger Point for serving

--- Assets/2.Code/InputController.cs
+++ Assets/2.Code/InputController.cs
@@ -6,19 +6,28 @@ using UnityEngine.InputSystem;
 public class InputController : MonoBehaviour
 {
     [SerializeField] private InputAction _moveInput = null;
+    [SerializeField] private InputAction _actionInput = null;
 
     private void OnEnable()
     {
         _moveInput.Enable();
+        _actionInput.Enable();
     }
 
     private void OnDisable()
     {
         _moveInput.Disable();
+        _actionInput.Enable();
     }
 
     public float MoveInput()
     {
         return _moveInput.ReadValue<float>();
+    }
+
+    public float ActionInput() 
+    {
+
+        return _actionInput.ReadValue<float>();
     }
 }
